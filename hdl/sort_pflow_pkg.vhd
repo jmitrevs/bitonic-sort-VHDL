11cbb615e1c2d28f96db712ce38667a6bcfe4b6b
Jovan Mitrevski <jmitrevs@fnal.gov>
2021-06-01 16:10:41 -0500

    add pflow sort

---
+++ hdl/sort_pflow_pkg.vhd
@@ -0,0 +1,11 @@
+
+library IEEE;
+use IEEE.std_logic_1164.all;
+
+package sort_pflow_pkg is
+  constant NUM_INPUTS : positive := 15;
+  constant NUM_INPUTS_PW2 : positive := 16;
+  constant NUM_OUTPUTS : positive := 10;
+  constant NUM_BITS : positive := 80;
+  constant COMP_BITS : positive := 16;
+end sort_pflow_pkg;
