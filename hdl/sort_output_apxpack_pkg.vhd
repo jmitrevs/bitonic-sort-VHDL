ee9b3bc92d6f5ca4cf7f55ee4448b580e1d67db6
Jovan Mitrevski <jmitrevs@fnal.gov>
2021-08-31 19:01:20 -0500

    update/define some constants

--- hdl/sort_output_apxpack_pkg.vhd
+++ hdl/sort_output_apxpack_pkg.vhd
@@ -6,6 +6,7 @@ package sort_output_apxpack_pkg is
   constant NUM_INPUTS : positive := 47;
   constant NUM_INPUTS_PW2 : positive := 64;
   constant NUM_OUTPUTS : positive := 18;
-  constant NUM_BITS : positive := 64;
-  constant COMP_BITS : positive := 16;
+  constant NUM_BITS : positive := 55;  -- though the input is 64-bit, only 55 are actually nonzero
+  constant COMP_BITS : positive := 14;
+  constant SORT_DELAY : positive := 17;  -- estimate, not confirmed
 end sort_output_apxpack_pkg;
--- hdl/sort_pflow_pkg.vhd
+++ hdl/sort_pflow_pkg.vhd
@@ -6,7 +6,7 @@ package sort_pflow_pkg is
   constant NUM_INPUTS : positive := 15;
   constant NUM_INPUTS_PW2 : positive := 16;
   constant NUM_OUTPUTS : positive := 10;
-  constant NUM_BITS : positive := 80;
-  constant COMP_BITS : positive := 16;
+  constant NUM_BITS : positive := 37;
+  constant COMP_BITS : positive := 14;
   constant SORT_DELAY : positive := 13;
 end sort_pflow_pkg;
